1175357b8399ba06c4a2f732478f0a51809c8a2f
re-katakata <taichi1207mocho@gmail.com>
2021-04-14 11:51:51 +0900

    create database/migrations

---
+++ database/migrations/2021_04_09_200922_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('schedule_number');
+            $table->bigInteger('credit');
+            $table->bigInteger('absence');
+            $table->string('name');
+            $table->string('place');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
