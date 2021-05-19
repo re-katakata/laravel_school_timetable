abe0b399cdef8c249d952a64953f1e019b70c665
re-katakata <taichi1207mocho@gmail.com>
2021-05-19 17:12:46 +0900

    first commit

--- resources/views/Index/index.blade.php
+++ resources/views/Index/index.blade.php
@@ -36,6 +36,7 @@
                             <div class="classDetail monday{{$j}}Detail">
                             
                                 
+                                
                             </div>
 
                         @endfor
